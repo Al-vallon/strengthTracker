644bd9507ffc24b34f887f746fe281ba00a63975
alex <81435330+Al-vallon@users.noreply.github.com>
2025-05-14 21:41:18 +0200

    Add initial backend structure with Maven configuration and Spring Boot setup

---
+++ backend/src/main/java/strengthTracker/strengthTracker/StrengthTrackerApplication.java
@@ -0,0 +1,13 @@
+package strengthTracker.strengthTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrengthTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/strengthTracker/strengthTracker/StrengthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package strengthTracker.strengthTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
