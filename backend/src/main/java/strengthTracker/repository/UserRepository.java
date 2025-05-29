cab88a4498699998f4394d4e0cc5120e3f62b979
alex <81435330+Al-vallon@users.noreply.github.com>
2025-05-29 22:57:08 +0200

    Add Swagger support and update UserController formatting
    - Introduced springdoc-openapi dependency in pom.xml for API documentation.
    - Added Swagger configuration class for API documentation setup.
    - Updated application.properties to configure Swagger paths.
    - Reformatted UserController for improved readability.

--- backend/src/main/java/strengthTracker/repository/UserRepository.java
+++ backend/src/main/java/strengthTracker/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package strengthtracker.repository;
 
-import strengthtracker.entity.User;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-
+import strengthtracker.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // Additional methods for user-specific queries can be added here
+  // Additional methods for user-specific queries can be added here
 
 }
